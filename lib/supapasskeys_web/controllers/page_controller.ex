531f00f7a8fe48eab79a67b3590a0752e3286c40
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-11-18 19:35:21 +0100

    Revert "Merge pull request #3 from jehrhardt/axum-poc"
    This reverts commit 7727fdc6dd9c520bd4c02b6bd79be004332478c0, reversing
    changes made to 729aecf56257ebf32f526c7b41daf0e0198c2d00.

---
+++ lib/supapasskeys_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule SupapasskeysWeb.PageController do
+  use SupapasskeysWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
