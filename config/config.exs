d6a4b7d7a7ef05ef81a4b6400aa80d63c0e9b685
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-22 15:09:07 +0100

    Add LiveView again
    The UI will be implemented with Phoenix LiveView. This allows to build a monolithic full-stack application, which is the most effective way to implement this.

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,13 @@
 # is restricted to this project.
 
 # General application configuration
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
 
 config :supapasskeys,
@@ -16,7 +23,7 @@ config :supapasskeys, SupapasskeysWeb.Endpoint,
   url: [host: "localhost"],
   adapter: Phoenix.Endpoint.Cowboy2Adapter,
   render_errors: [
-    formats: [json: SupapasskeysWeb.ErrorJSON],
+    formats: [html: SupapasskeysWeb.ErrorHTML, json: SupapasskeysWeb.ErrorJSON],
     layout: false
   ],
   pubsub_server: Supapasskeys.PubSub,
@@ -30,6 +37,28 @@ config :supapasskeys, SupapasskeysWeb.Endpoint,
 # For production it's recommended to configure a different adapter
 # at the `config/runtime.exs`.
 config :supapasskeys, Supapasskeys.Mailer, adapter: Swoosh.Adapters.Local
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.3.2",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,10 @@ config :supapasskeys, SupapasskeysWeb.Endpoint,
   code_reloader: true,
   debug_errors: true,
   secret_key_base: "gy4dOrW6xHpl/tiM7Pd8utPXuxA+uyxA6grDFPd5rY0q+BHYJ7w5ByelSkI9qGxA",
-  watchers: []
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
+  ]
 
 # ## SSL Support
 #
@@ -50,6 +53,16 @@ config :supapasskeys, SupapasskeysWeb.Endpoint,
 # configured to run both http and https servers on
 # different ports.
 
+# Watch static and templates for browser reloading.
+config :supapasskeys, SupapasskeysWeb.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"priv/gettext/.*(po)$",
+      ~r"lib/supapasskeys_web/(controllers|live|components)/.*(ex|heex)$"
+    ]
+  ]
+
 # Enable dev routes for dashboard and mailbox
 config :supapasskeys, dev_routes: true
 
@@ -62,6 +75,9 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Include HEEx debug annotations as HTML comments in rendered markup
+config :phoenix_live_view, :debug_heex_annotations, true
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
