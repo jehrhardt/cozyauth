95fca6258dedc6bd590438ee2fb76dc0c93acca9
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-27 18:20:18 +0100

    Fix Elixir formatting

--- lib/supapasskeys/webauthn.ex
+++ lib/supapasskeys/webauthn.ex
@@ -1,6 +1,6 @@
 defmodule Supapasskeys.WebAuthn do
-    use Rustler, otp_app: :supapasskeys, crate: "supapasskeys_webauthn"
+  use Rustler, otp_app: :supapasskeys, crate: "supapasskeys_webauthn"
 
-    # When your NIF is loaded, it will override this function.
-    def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-  end
+  # When your NIF is loaded, it will override this function.
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+end
