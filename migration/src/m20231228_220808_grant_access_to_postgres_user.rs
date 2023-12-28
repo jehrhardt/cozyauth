b8a4c039c19e563359b300b857d4e5f2d83a053e
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-28 23:18:23 +0100

    Setup supapasskeys schema and user
    Supapasskeys will store data in the supapasskeys schema and use the supapasskeys user, which does not have full access to the DB.

---
+++ migration/src/m20231228_220808_grant_access_to_postgres_user.rs
@@ -0,0 +1,15 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .get_connection()
+            .execute_unprepared("GRANT SELECT ON seaql_migrations TO postgres;")
+            .await?;
+        Ok(())
+    }
+}
