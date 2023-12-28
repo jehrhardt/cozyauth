4344dc9fc1cb18a87e877c0d594715eb5cc92f54
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-28 20:11:50 +0100

    Revert "Merge pull request #22 from jehrhardt/dependabot/hex/phoenix_html-4.0.0"
    This reverts commit efafccbb579afe0b545cb4537887e1efa8000764, reversing
    changes made to f4fa82246c958bdaaa3f0a8fef568b6a7619073d.

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Supapasskeys.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.0"},
+      {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.1"},
       {:floki, ">= 0.30.0", only: :test},
