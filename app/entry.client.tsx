6b49444f0dda71deafa30f69bef81742f739d243
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-29 17:57:54 +0100

    Update deno import map
    Most packages work with NPM compatibility.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,7 +5,7 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { React, startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -7,6 +7,7 @@
 import type { AppLoadContext, EntryContext } from "@remix-run/deno";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
+import React from "react";
 import { renderToReadableStream } from "react-dom/server";
 
 export default async function handleRequest(
