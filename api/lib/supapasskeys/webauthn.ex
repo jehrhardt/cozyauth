67842145b3a79123bdd002d192c598625eb94d0b
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2023-12-31 18:02:04 +0100

    Add Passkey registration bindings

--- api/lib/supapasskeys/webauthn.ex
+++ api/lib/supapasskeys/webauthn.ex
@@ -1,7 +1,17 @@
 defmodule Supapasskeys.WebAuthn do
   use Rustler, otp_app: :supapasskeys, crate: "supapasskeys_webauthn"
 
+  defmodule RelyingParty do
+    defstruct name: nil, origin: nil
+  end
+
+  defmodule User do
+    defstruct id: nil, name: nil, display_name: nil
+  end
+
   # When your NIF is loaded, it will override this function.
-  @spec add(Integer.t(), Integer.t()) :: Integer.t()
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def start_passkey_registration(_relying_party, _user), do: :erlang.nif_error(:nif_not_loaded)
+
+  def finish_passkey_registration(_relying_party, _public_key_credential_json, _state_json),
+    do: :erlang.nif_error(:nif_not_loaded)
 end
---
+++ api/test/supapasskeys/webauthn_test.exs
@@ -0,0 +1,69 @@
+defmodule Supapasskeys.WebAuthnTest do
+  use ExUnit.Case
+
+  alias Supapasskeys.WebAuthn
+
+  describe "WebAuthn" do
+    test "start_passkey_registration/2 returns credentials creation options" do
+      relying_party_domain = Faker.Internet.domain_name()
+
+      relying_party =
+        %WebAuthn.RelyingParty{
+          name: Faker.Company.name(),
+          origin: "https://#{relying_party_domain}"
+        }
+
+      user =
+        %WebAuthn.User{
+          id: Faker.UUID.v4(),
+          name: Faker.Internet.email(),
+          display_name: Faker.Person.name()
+        }
+
+      {credentials_creation_options, state} =
+        WebAuthn.start_passkey_registration(relying_party, user)
+
+      %{
+        "publicKey" => %{
+          "rp" => %{
+            "name" => relying_party_name,
+            "id" => relying_party_id
+          },
+          "user" => %{
+            "name" => user_name,
+            "displayName" => user_display_name,
+            "id" => user_id
+          },
+          "challenge" => challenge,
+          "pubKeyCredParams" => [
+            %{"type" => "public-key", "alg" => -7},
+            %{"type" => "public-key", "alg" => -257}
+          ],
+          "timeout" => 60000,
+          "attestation" => "none",
+          "authenticatorSelection" => %{
+            "requireResidentKey" => false,
+            "userVerification" => "preferred"
+          },
+          "extensions" => %{"uvm" => true, "credProps" => true}
+        }
+      } = Jason.decode!(credentials_creation_options)
+
+      assert relying_party_name == relying_party.name
+      assert relying_party_id == relying_party_domain
+      assert user_name == user.name
+      assert user_display_name == user.display_name
+
+      assert user_id ==
+               UUID.string_to_binary!(user.id)
+               |> Base.url_encode64(padding: false)
+
+      assert is_binary(challenge)
+
+      assert Base.url_decode64!(challenge, padding: false)
+             |> byte_size() == 32
+
+      assert is_binary(state)
+    end
+  end
+end
