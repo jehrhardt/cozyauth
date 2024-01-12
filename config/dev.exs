ab96c0a743cb1b0254e04b92274e64146e84be0f
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-01-13 00:13:24 +0100

    Use Postgres 15 from Supabase
    Although Supapasskeys can be used with plain Postgres. Using the Supabase Postgres images provides a similar environment than Supabase.

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :supapasskeys, Supapasskeys.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "supapasskeys_dev",
+  database: "postgres",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
