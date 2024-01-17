2fecbb7450675d999cfecca1b983cad8885122da
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-01-17 22:16:35 +0100

    Add tests for API Auth Plug

--- lib/supapasskeys_web/plugs/api_auth.ex
+++ lib/supapasskeys_web/plugs/api_auth.ex
@@ -4,35 +4,46 @@ defmodule SupapasskeysWeb.Plugs.ApiAuth do
   import Plug.Conn
 
   @impl true
-  def init(opts), do: opts
-
-  @impl true
-  def call(conn, _opts) do
+  def init(options) do
     api_domain =
       Application.get_env(:supapasskeys, SupapasskeysWeb.Plugs.ApiAuth)
       |> Keyword.get(:api_domain)
 
-    server =
-      conn.host
-      |> String.replace(".#{api_domain}", "")
-      |> Passkeys.get_server_by_subdomain()
+    options
+    |> Keyword.put(:api_domain, api_domain)
+  end
 
-    case server do
-      nil ->
-        conn
-        |> put_status(404)
-        |> put_resp_header("content-type", "application/json")
-        |> Phoenix.Controller.json(%{error: "Invalid API domain"})
-        |> halt()
+  @impl true
+  def call(conn, options) do
+    if !String.ends_with?(conn.host, options[:api_domain]) do
+      conn
+      |> put_status(404)
+      |> put_resp_header("content-type", "application/json")
+      |> Phoenix.Controller.json(%{error: "Invalid Subdomain domain"})
+      |> halt()
+    else
+      server =
+        conn.host
+        |> String.replace(".#{options[:api_domain]}", "")
+        |> Passkeys.get_server_by_subdomain()
 
-      _ ->
-        conn
-        |> put_req_header("x-supapasskeys-server-id", server.id)
-        |> put_req_header("x-supapasskeys-server-relying-party-name", server.relying_party_name)
-        |> put_req_header(
-          "x-supapasskeys-server-relying-party-origin",
-          server.relying_party_origin
-        )
+      case server do
+        nil ->
+          conn
+          |> put_status(404)
+          |> put_resp_header("content-type", "application/json")
+          |> Phoenix.Controller.json(%{error: "Invalid API domain"})
+          |> halt()
+
+        _ ->
+          conn
+          |> put_req_header("x-supapasskeys-server-id", server.id)
+          |> put_req_header("x-supapasskeys-server-relying-party-name", server.relying_party_name)
+          |> put_req_header(
+            "x-supapasskeys-server-relying-party-origin",
+            server.relying_party_origin
+          )
+      end
     end
   end
 end
---
+++ test/supapasskeys_web/plugs/api_auth_test.exs
@@ -0,0 +1,53 @@
+defmodule SupapasskeysWeb.Plugs.ApiAuthTest do
+  alias Supapasskeys.Passkeys
+  use SupapasskeysWeb.ConnCase
+
+  setup %{conn: conn} do
+    import Supapasskeys.PasskeysFixtures
+    server = server_fixture()
+
+    on_exit(fn ->
+      Passkeys.delete_server(server)
+    end)
+
+    {:ok,
+     conn:
+       put_req_header(conn, "accept", "application/json")
+       |> Map.put(:host, "#{server.subdomain}.example.com"),
+     server: server}
+  end
+
+  describe "call/2" do
+    test "returns 404 Not Found when subdomain is not found", %{conn: conn} do
+      invalid_conn = Map.put(conn, :host, "#{Faker.Internet.domain_word()}.example.com")
+      conn = SupapasskeysWeb.Plugs.ApiAuth.call(invalid_conn, api_domain: "example.com")
+      assert json_response(conn, 404)["error"] == "Invalid API domain"
+    end
+
+    test "returns 404 Not Found when api domain is incorrect", %{conn: conn} do
+      conn = SupapasskeysWeb.Plugs.ApiAuth.call(conn, api_domain: "incorrect.com")
+      assert json_response(conn, 404)["error"] == "Invalid Subdomain domain"
+    end
+
+    test "adds server ID and relying party when a valid subdomain is provided", %{
+      conn: conn,
+      server: server
+    } do
+      conn = SupapasskeysWeb.Plugs.ApiAuth.call(conn, api_domain: "example.com")
+      assert conn.status != 404
+      assert get_req_header(conn, "x-supapasskeys-server-id") |> List.first() == server.id
+
+      assert get_req_header(conn, "x-supapasskeys-server-relying-party-name") |> List.first() ==
+               server.relying_party_name
+
+      assert get_req_header(conn, "x-supapasskeys-server-relying-party-origin") |> List.first() ==
+               server.relying_party_origin
+    end
+  end
+
+  describe "init/1" do
+    test "sets api domain from config" do
+      assert SupapasskeysWeb.Plugs.ApiAuth.init([])[:api_domain] == "example.com"
+    end
+  end
+end
