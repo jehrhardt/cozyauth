7cab9afa51f2dddf00881a221d5f015d377c3b08
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-02-05 18:35:54 +0100

    Switch to Bandint

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :supapasskeys,
 # Configures the endpoint
 config :supapasskeys, SupapasskeysWeb.Endpoint,
   url: [host: "localhost"],
-  adapter: Phoenix.Endpoint.Cowboy2Adapter,
+  adapter: Bandit.PhoenixAdapter,
   render_errors: [
     formats: [html: SupapasskeysWeb.ErrorHTML, json: SupapasskeysWeb.ErrorJSON],
     layout: false
@@ -34,7 +34,7 @@ config :supapasskeys, Supapasskeys.Mailer, adapter: Swoosh.Adapters.Local
 # Configure esbuild (the version is required)
 config :esbuild,
   version: "0.17.11",
-  default: [
+  supapasskeys: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
@@ -43,8 +43,8 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.3.2",
-  default: [
+  version: "3.4.0",
+  supapasskeys: [
     args: ~w(
       --config=tailwind.config.js
       --input=css/app.css
