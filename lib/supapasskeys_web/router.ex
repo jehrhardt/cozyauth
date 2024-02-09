29770de129619af18b5674fc651dd30f21bd7bd3
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-02-09 07:37:29 +0100

    Remove unused Supabase context

--- lib/supapasskeys_web/router.ex
+++ lib/supapasskeys_web/router.ex
@@ -19,13 +19,6 @@ defmodule SupapasskeysWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-
-    live "/projects", ProjectLive.Index, :index
-    live "/projects/new", ProjectLive.Index, :new
-    live "/projects/:id/edit", ProjectLive.Index, :edit
-
-    live "/projects/:id", ProjectLive.Show, :show
-    live "/projects/:id/show/edit", ProjectLive.Show, :edit
   end
 
   scope "/passkeys", SupapasskeysWeb do
