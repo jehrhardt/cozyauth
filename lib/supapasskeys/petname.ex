6ea25a2b9113e339e8b7377e3857a3aeaab76c0f
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-02-24 08:20:56 +0100

    Remove Elixir code base
    The plan is to rewrite the API in Rust and therefore drop all the Elixir
    code.

--- lib/supapasskeys/petname.ex
+++
@@ -1,6 +0,0 @@
-defmodule Supapasskeys.Petname do
-  use Rustler, otp_app: :supapasskeys, crate: "supapasskeys_petname"
-
-  # When your NIF is loaded, it will override this function.
-  def generate_subdomain(), do: :erlang.nif_error(:nif_not_loaded)
-end
--- lib/supapasskeys_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule SupapasskeysWeb.PageHTML do
-  use SupapasskeysWeb, :html
-
-  embed_templates "page_html/*"
-end
