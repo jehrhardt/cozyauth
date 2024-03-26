11980717be21da6718f8acf48fb9fe04bb2da4cd
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-03-26 07:42:29 +0100

    Fix cozyauth-server package name
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- cozyauth-server/src/bin/cozyauth-server/main.rs
+++ cozyauth-server/src/bin/cozyauth-server/main.rs
@@ -1,7 +1,7 @@
 // © Copyright 2024 Jan Ehrhardt
 // SPDX-License-Identifier: AGPL-3.0-or-later OR Apache-2.0
 
-use cozyauth::app;
+use cozyauth_server::app;
 use tokio::net::TcpListener;
 
 #[tokio::main]
