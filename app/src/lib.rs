ee8fa79f801bd51b84c705c0d3332048f6656cdc
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-04-21 12:31:47 +0200

    Remove Pavex setup
    We will go back to axum for the application as it does not have as many
    constraints on the project structure.
    
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- app/src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod blueprint;
-pub mod configuration;
-pub mod routes;
-pub mod telemetry;
-
-pub use blueprint::blueprint;
--- app/src/routes/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod health;
-
-use pavex::blueprint::{router::GET, Blueprint};
-use pavex::f;
-
-pub fn register(bp: &mut Blueprint) {
-    bp.route(GET, "/health", f!(self::health::get));
-}
