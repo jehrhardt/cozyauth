0994d77f2db04bb1376cacd0bf6662a5f06c9012
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-04-21 12:56:24 +0200

    Cleanup server porject to use axum again
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

---
+++ server/src/bin/cozyauth-server/main.rs
@@ -0,0 +1,33 @@
+// © Copyright 2024 Jan Ehrhardt
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use server::app;
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::{self, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "cozyauth_server=debug,axum::rejection=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let app = app::app();
+    let ip_address: IpAddr = if cfg!(debug_assertions) {
+        Ipv4Addr::LOCALHOST.into()
+    } else {
+        Ipv4Addr::UNSPECIFIED.into()
+    };
+    let socket_address = SocketAddr::new(ip_address, app::server_port());
+    let listener = TcpListener::bind(&socket_address).await.unwrap();
+    info!("Listening on {}", socket_address);
+    axum::serve(listener, app.into_make_service())
+        .with_graceful_shutdown(app::shutdown_signal())
+        .await
+        .unwrap()
+}
