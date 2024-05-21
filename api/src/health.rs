bf312f2a0f1d50e394214cfe0cd52f2dec1929a7
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-05-21 06:36:34 +0200

    Add missing test
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- api/src/health.rs
+++ api/src/health.rs
@@ -1,8 +1,50 @@
 // © Copyright 2024 the cozyauth developers
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::json;
 
 pub(crate) fn router() -> Router {
-    Router::new().route("/health", get(|| async { "OK" }))
+    Router::new().route("/health", get(|| async { Json(json!({ "status": "✅" })) }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{self, header, HeaderValue, Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use serde_json::{json, Value};
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn status_ok() {
+        let app = router();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/health")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let expected_content_type =
+            HeaderValue::from_str(mime::APPLICATION_JSON.to_string().as_str()).unwrap();
+        assert_eq!(
+            response.headers().get(header::CONTENT_TYPE),
+            Some(&expected_content_type)
+        );
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({"status": "✅"}));
+    }
 }
