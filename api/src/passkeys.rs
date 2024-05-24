83b54a42a7ebdba19f506700a02002658ab9f6c5
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-05-24 07:01:50 +0200

    Fix tests
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- api/src/passkeys.rs
+++ api/src/passkeys.rs
@@ -1,14 +1,19 @@
 // © Copyright 2024 the cozyauth developers
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use axum::{routing::post, Json, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use serde::Deserialize;
 use uuid::Uuid;
-use webauthn_rs::{
-    prelude::{CreationChallengeResponse, Url},
-    WebauthnBuilder,
-};
+use webauthn_rs::{prelude::Url, WebauthnBuilder};
 
-async fn create() -> Json<CreationChallengeResponse> {
+#[derive(Deserialize)]
+struct UserParams {
+    id: Uuid,
+    name: String,
+    display_name: Option<String>,
+}
+
+async fn create(Json(user): Json<UserParams>) -> impl IntoResponse {
     let rp_id = "example.com";
     let rp_origin = Url::parse("https://idm.example.com").unwrap();
     let webauthn = WebauthnBuilder::new(rp_id, &rp_origin)
@@ -16,9 +21,14 @@ async fn create() -> Json<CreationChallengeResponse> {
         .build()
         .unwrap();
     let (x, _) = webauthn
-        .start_passkey_registration(Uuid::new_v4(), "foo", "Foo", None)
+        .start_passkey_registration(
+            user.id,
+            &user.name,
+            &user.display_name.unwrap_or(user.name.to_string()),
+            None,
+        )
         .unwrap();
-    Json(x)
+    (StatusCode::ACCEPTED, Json(x))
 }
 
 pub(crate) fn router() -> Router {
@@ -30,16 +40,18 @@ mod tests {
     use super::*;
     use axum::{
         body::Body,
-        http::{self, header, HeaderValue, Request, StatusCode},
+        http::{self, header, HeaderValue, Request},
     };
     use http_body_util::BodyExt;
-    use serde_json::{json, Value};
+    use serde_json::json;
     use tower::ServiceExt;
+    use webauthn_rs::prelude::{Base64UrlSafeData, CreationChallengeResponse};
 
     #[tokio::test]
     async fn simple_request() {
         let app = router();
 
+        let user_id = Uuid::new_v4();
         let response = app
             .oneshot(
                 Request::builder()
@@ -47,7 +59,7 @@ mod tests {
                     .uri("/")
                     .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
                     .body(Body::from(
-                        serde_json::to_vec(&json!({ "id": "", "name": "Foo" })).unwrap(),
+                        serde_json::to_vec(&json!({ "id": user_id, "name": "Foo" })).unwrap(),
                     ))
                     .unwrap(),
             )
@@ -64,7 +76,10 @@ mod tests {
         );
 
         let body = response.into_body().collect().await.unwrap().to_bytes();
-        let body: Value = serde_json::from_slice(&body).unwrap();
-        assert_eq!(body, json!({"status": "✅"}));
+        let options: CreationChallengeResponse = serde_json::from_slice(&body).unwrap();
+        let user_id_encoded: Base64UrlSafeData = user_id.as_bytes().into();
+        assert_eq!(options.public_key.user.id, user_id_encoded);
+        assert_eq!(options.public_key.user.name, "Foo");
+        assert_eq!(options.public_key.user.display_name, "Foo");
     }
 }
