1c585c338c0cec478f7cfacaacaddfee0bcdf65e
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-07-13 13:55:36 +0200

    Add missing test for health endpoint
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- app/src/api/health.rs
+++ app/src/api/health.rs
@@ -7,3 +7,47 @@ use serde_json::json;
 pub(crate) fn router() -> Router {
     Router::new().route("/health", get(|| async { Json(json!({ "status": "✅" })) }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use serde_json::Value;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn health_check() {
+        let app = router();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/health")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            response
+                .headers()
+                .get(http::header::CONTENT_TYPE)
+                .unwrap()
+                .to_str()
+                .unwrap(),
+            mime::APPLICATION_JSON
+        );
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "status": "✅" }));
+    }
+}
