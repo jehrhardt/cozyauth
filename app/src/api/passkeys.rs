0ae39fac9b8dd93d3b67aa6b389b1ea75572959e
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-07-22 09:19:39 +0200

    Return correct JSON output
    The publicKey attribute is not part of the JSON representation.
    
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- app/src/api/passkeys.rs
+++ app/src/api/passkeys.rs
@@ -7,10 +7,11 @@ use uuid::Uuid;
 use webauthn_rs::prelude::*;
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct User {
     id: Uuid,
     name: String,
-    display_name: String,
+    display_name: Option<String>,
 }
 
 async fn create(Json(user): Json<User>) -> impl IntoResponse {
@@ -28,8 +29,11 @@ async fn create(Json(user): Json<User>) -> impl IntoResponse {
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
         .unwrap();
 
-    match webauthn.start_passkey_registration(user.id, &user.name, &user.display_name, None) {
-        Ok((ccr, _skr)) => (StatusCode::ACCEPTED, Json(ccr)).into_response(),
+    let user_name = user.name.as_str();
+    let user_display_name = user.display_name.unwrap_or_else(|| user_name.to_string());
+
+    match webauthn.start_passkey_registration(user.id, user_name, &user_display_name, None) {
+        Ok((ccr, _skr)) => (StatusCode::ACCEPTED, Json(ccr.public_key)).into_response(),
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
@@ -46,7 +50,9 @@ mod tests {
         body::Body,
         http::{self, Request, StatusCode},
     };
+    use base64::{engine::general_purpose, Engine};
     use http_body_util::BodyExt;
+    use serde_json::Value;
     use tower::ServiceExt;
 
     #[tokio::test]
@@ -55,7 +61,7 @@ mod tests {
         let user = User {
             id: Uuid::new_v4(),
             name: "alice".to_string(),
-            display_name: "Alice".to_string(),
+            display_name: Some("Alice".to_string()),
         };
         let response = app
             .oneshot(
@@ -81,10 +87,104 @@ mod tests {
         );
 
         let body = response.into_body().collect().await.unwrap().to_bytes();
-        let body: CreationChallengeResponse = serde_json::from_slice(&body).unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
 
-        assert_eq!(body.public_key.rp.id, "localhost");
-        assert_eq!(body.public_key.user.name, user.name);
-        assert_eq!(body.public_key.user.display_name, user.display_name);
+        assert_credential_creation_options(user, body)
+    }
+
+    fn assert_credential_creation_options(user: User, json: Value) {
+        let rp = json.get("rp").unwrap();
+        assert_eq!(rp.get("id").and_then(|v| v.as_str()), Some("localhost"));
+
+        let user_json = json.get("user").unwrap();
+        let user_id = user.id.as_bytes().to_vec();
+        assert_eq!(
+            user_json
+                .get("id")
+                .map(|v| v.as_str().unwrap())
+                .map(|v| general_purpose::URL_SAFE_NO_PAD.decode(v).unwrap()),
+            Some(user_id)
+        );
+
+        assert_eq!(
+            user_json.get("name").and_then(|v| v.as_str()),
+            Some(user.name.as_str())
+        );
+
+        let display_name = user.display_name.unwrap();
+        assert_eq!(
+            user_json.get("displayName").and_then(|v| v.as_str()),
+            Some(display_name.as_str())
+        );
+
+        assert!(json
+            .get("challenge")
+            .map(|v| v.as_str().unwrap())
+            .map(|v| general_purpose::URL_SAFE_NO_PAD.decode(v).is_ok())
+            .unwrap());
+
+        let pub_key_credentials_params = json
+            .get("pubKeyCredParams")
+            .map(|v| v.as_array().unwrap())
+            .unwrap();
+        assert_eq!(pub_key_credentials_params.len(), 2);
+        assert_eq!(
+            pub_key_credentials_params
+                .first()
+                .and_then(|v| v.get("alg").unwrap().as_i64()),
+            Some(-7)
+        );
+        assert_eq!(
+            pub_key_credentials_params
+                .get(1)
+                .and_then(|v| v.get("alg").unwrap().as_i64()),
+            Some(-257)
+        );
+
+        assert_eq!(json.get("timeout").and_then(|v| v.as_u64()), Some(300000));
+
+        let authenticator_selection = json.get("authenticatorSelection").unwrap();
+        assert_eq!(
+            authenticator_selection
+                .get("residentKey")
+                .and_then(|v| v.as_str()),
+            Some("discouraged")
+        );
+        assert_eq!(
+            authenticator_selection
+                .get("requireResidentKey")
+                .and_then(|v| v.as_bool()),
+            Some(false)
+        );
+        assert_eq!(
+            authenticator_selection
+                .get("userVerification")
+                .and_then(|v| v.as_str()),
+            Some("required")
+        );
+
+        assert_eq!(
+            json.get("attestation").and_then(|v| v.as_str()),
+            Some("none")
+        );
+
+        let extensions = json.get("extensions").unwrap();
+        assert_eq!(
+            extensions
+                .get("credentialProtectionPolicy")
+                .and_then(|v| v.as_str()),
+            Some("userVerificationRequired")
+        );
+        assert_eq!(
+            extensions
+                .get("enforceCredentialProtectionPolicy")
+                .and_then(|v| v.as_bool()),
+            Some(false)
+        );
+        assert_eq!(extensions.get("uvm").and_then(|v| v.as_bool()), Some(true));
+        assert_eq!(
+            extensions.get("credProps").and_then(|v| v.as_bool()),
+            Some(true)
+        );
     }
 }
