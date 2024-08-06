54c03663adf3fec720dd365ca4fb8864237b5ceb
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-08-06 16:15:38 +0200

    Make migrate a subcommand of cozyauth db
    `cozyauth db` will provide all Postgres related subcommands. Migrate is
    one of them and must be moved to become a subcommand.
    
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 use crate::{app::start_server, config::Settings, db::migrate};
 
@@ -17,6 +17,18 @@ enum Commands {
         long_about = "Starts the server on a port configured via PORT environment variable or 3000 as default port."
     )]
     Server,
+    Db(DbArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(flatten_help = true)]
+struct DbArgs {
+    #[command(subcommand)]
+    command: DbCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum DbCommands {
     #[command(
         about = "Manage the database schema",
         long_about = "Manage the database schema of the Postgres database configured via the DATABASE_URL variable.\n\
@@ -30,6 +42,8 @@ pub async fn run() {
     let settings = Settings::from_env();
     match args.command {
         Commands::Server => start_server(settings).await,
-        Commands::Migrate => migrate(&settings).await,
+        Commands::Db(db) => match db.command {
+            DbCommands::Migrate => migrate(&settings).await,
+        },
     }
 }
