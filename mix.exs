de89bb75cf433929488d60bd7509738a2ccfb42a
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2025-01-04 08:47:21 +0100

    Use Elixir 1.18
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CozyAuth.MixProject do
     [
       app: :cozyauth,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
